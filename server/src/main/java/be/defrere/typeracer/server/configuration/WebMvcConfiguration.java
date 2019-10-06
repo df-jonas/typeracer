972c02c2401fb8df7ccb0f1a5f2cbf1f93644872
Jonas De Frère <defrere.jonas@gmail.com>
2019-10-06 21:44:33 +0200

    allow disabling CORS on dev profile

--- server/src/main/java/be/defrere/typeracer/server/configuration/WebMvcConfiguration.java
+++ server/src/main/java/be/defrere/typeracer/server/configuration/WebMvcConfiguration.java
@@ -1,8 +1,11 @@
 package be.defrere.typeracer.server.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,11 +13,20 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
 
+import static java.util.Arrays.asList;
+
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     private static final String FRONTEND_INDEX = "index.html";
     private static final String FRONTEND_RESOURCES = "/META-INF/resources/ui/";
+
+    private final Environment environment;
+
+    @Autowired
+    public WebMvcConfiguration(Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -38,5 +50,13 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                         return requestedResource.exists() && requestedResource.isReadable();
                     }
                 });
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // disable CORS when developing
+        if (asList(environment.getActiveProfiles()).contains("dev")) {
+            registry.addMapping("**").allowedOrigins("**");
+        }
     }
 }
